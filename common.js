141f2fc42f64980b9554d5519ccd6e51666ba562
GreatCorn <lolinyourhouse@gmail.com>
2020-10-31 17:37:01 +0200

    Fixed directory, added Bleaks

--- common.js
+++ common.js
@@ -1,11 +1,11 @@
 function Start(){
-	document.body.innerHTML = '<DIV id=navbar class=NavBar><A href="/"><IMG id=homePageImg style="left: 0; position: absolute;" src="/banner.png"></A><A href="/about" class=NavBarLink>ABOUT</A><DIV class=VR></DIV><A href="/work" class=NavBarLink>MY WORK</A><DIV class=VR></DIV><A href="/friends" class=NavBarLink>FRIENDS</A><DIV class=VR></DIV><A href="/contacts" class=NavBarLink>CONTACTS</A></DIV><DIV class=Content>'+document.body.innerHTML+'</DIV><HR><SMALL>The work presented on this site has individual license per piece. The licenses are presented on the work pages.</SMALL>';
+	document.body.innerHTML = '<DIV id=navbar class=NavBar><A href="/me"><IMG id=homePageImg style="left: 0; position: absolute;" src="/me/banner.png"></A><A href="/me/about" class=NavBarLink>ABOUT</A><DIV class=VR></DIV><A href="/me/work" class=NavBarLink>MY WORK</A><DIV class=VR></DIV><A href="/me/friends" class=NavBarLink>FRIENDS</A><DIV class=VR></DIV><A href="/me/contacts" class=NavBarLink>CONTACTS</A></DIV><DIV class=Content>'+document.body.innerHTML+'</DIV><HR><SMALL>The work presented on this site has individual license per piece. The licenses are presented on the work pages.</SMALL>';
 	/*var navBar = document.createElement('div');
 	navBar.className = 'NavBar';
 	document.body.appendChild(navBar);
 	*/
 	if (window.innerWidth < 828)
-		document.getElementById("homePageImg").src = "/bannerCorn.png"
+		document.getElementById("homePageImg").src = "/me/bannerCorn.png"
 	var preview = document.getElementById("preview");
 	if (preview){
 		preview.height = document.getElementById("description").offsetHeight;
