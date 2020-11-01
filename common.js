4ee9e45a1693b77f95329922b666ca1fc4a53f89
GreatCorn <lolinyourhouse@gmail.com>
2020-11-01 13:14:27 +0200

    Added more work and contacts page

--- common.js
+++ common.js
@@ -31,6 +31,9 @@ function Start(){
 		modal.className = "Modal";
 		modal.onclick = function(){this.style.display = "none";}
 		document.body.appendChild(modal);
+		var modalHelper = document.createElement("span");
+		modalHelper.className = "ModalHelper";
+		modal.appendChild(modalHelper);
 		var modalIMG = document.createElement("img");
 		modalIMG.className = "ModalContent";
 		modalIMG.onclick = function(){window.open(this.src, "_blank");}
@@ -40,6 +43,45 @@ function Start(){
 				modal.style.display = "block";
 				modalIMG.src = this.src;
 			});
+		}
+	}
+	toggler = document.getElementsByClassName("TableSorter");
+	for (i = 0; i < toggler.length; i++) {
+		toggler[i].asc = false;
+		toggler[i].addEventListener("click", function() {
+			this.asc = !this.asc;
+			SortTable(this.parentElement.parentElement.parentElement, Array.prototype.indexOf.call(this.parentNode.children, this), this.asc);
+		});
+	}
+}
+
+function SortTable(table, n, asc) {
+	console.log("Sort", n, asc);
+	var rows, switching, i, x, y, shouldSwitch;
+	switching = true;
+	while (switching) {
+		switching = false;
+		rows = table.rows;
+		for (i = 1; i < (rows.length - 1); i++) {
+			shouldSwitch = false;
+			x = rows[i].getElementsByTagName("TD")[n];
+			y = rows[i + 1].getElementsByTagName("TD")[n];
+			if (asc){
+				if (x.innerText.toLowerCase() > y.innerText.toLowerCase()) {
+					shouldSwitch = true;
+					break;
+				}
+			}
+			else{
+				if (x.innerText.toLowerCase() < y.innerText.toLowerCase()) {
+					shouldSwitch = true;
+					break;
+				}
+			}
+		}
+		if (shouldSwitch) {
+			rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+			switching = true;
 		}
 	}
 }
